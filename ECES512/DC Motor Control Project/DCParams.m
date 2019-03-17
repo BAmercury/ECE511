57cfc8937c37a6ff675ab380b60cc7969df55a38
BrianAmin <tuf49787@temple.edu>
2019-03-16 21:30:34 -0400

    Added state feedback model

--- ECES512/DC Motor Control Project/DCParams.m
+++ ECES512/DC Motor Control Project/DCParams.m
@@ -15,3 +15,30 @@ arg = linmod('DC_motor_model')
 [NUM, DEN] = ss2tf(arg.a, arg.b, arg.c, arg.d);
 tfsys = tf(NUM, DEN)
 step(tf_sys)
+
+%%
+% Implementing a state feedback controller
+
+%%
+% Check if we are controllable:
+Cont = ctrb(arg.a, arg.b);
+rank(Cont)
+
+%%
+% Perform pole placement
+K = place(arg.a,arg.b, [-500 -600])
+K1 = K(1);
+K2 = K(2);
+
+%%
+% Get new closed loop transfer function
+cl_arg = linmod('DC_motor_model')
+[NUM, DEN] = ss2tf(cl_arg.a, cl_arg.b, cl_arg.c, cl_arg.d);
+cl_tfsys = tf(NUM, DEN)
+%%
+% Check poles of the new closed loop transfer function
+P = pole(cl_tfsys)
+
+%%
+% Step Response
+step(cl_tfsys)
